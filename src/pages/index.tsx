1e36486986b310c1ca4c6e0d0fbcb94f2d8e2628
Alfredo Henrique <thealfredohenrique@gmail.com>
2023-07-18 22:20:34 -0300

    feat(carousel): Add carousel to show products

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,23 @@
 import Image from "next/image";
+import { useKeenSlider } from "keen-slider/react";
 import { HomeContainer, Product } from "../styles/pages/home";
 import firstShirt from "../assets/1.png";
 import secondShirt from "../assets/2.png";
 import thirdShirt from "../assets/3.png";
+import "keen-slider/keen-slider.min.css";
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 2,
+      spacing: 48,
+    },
+  });
+
   return (
-    <HomeContainer>
-      <Product>
-        <Image src={firstShirt} width={390} height={360} alt="" />
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={firstShirt} width={520} height={480} alt="" />
 
         <footer>
           <strong>Camiseta Beyond the Limits</strong>
@@ -16,8 +25,26 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
-        <Image src={firstShirt} width={390} height={360} alt="" />
+      <Product className="keen-slider__slide">
+        <Image src={secondShirt} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta Beyond the Limits</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={thirdShirt} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta Beyond the Limits</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={thirdShirt} width={520} height={480} alt="" />
 
         <footer>
           <strong>Camiseta Beyond the Limits</strong>
